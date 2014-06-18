c5adf5c4d56c808c2dafbe24d83db8b377c49d9d
Michael Mitchell <michael.mitchell@getg5.com>
2014-06-18 16:02:52 -0700

    dont run integration specs by default

--- spec/integration/location_page_spec.rb
+++ spec/integration/location_page_spec.rb
@@ -1,6 +1,7 @@
 require "spec_helper"
 
-describe "Integration '/:website_slug/:web_page_template_slug'", :auth_request, js: true, vcr: VCR_OPTIONS do
+describe "Integration '/:website_slug/:web_page_template_slug'",
+         auth_request: true, integration: true, js: true, vcr: VCR_OPTIONS do
   before do
     VCR.use_cassette("Gardens") do
       GardenWebLayoutUpdater.new.update_all
