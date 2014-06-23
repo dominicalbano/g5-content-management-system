d91110b7439882f766bd33ff826019078db35af4
bbauer <brian.bauer@g5platform.com>
2014-06-23 10:58:17 -0700

    Migration to add location navigation setting
    The setting was added by JLS in the website seeder but she forgot the migration
    to add to existing sites.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140623165853) do
+ActiveRecord::Schema.define(version: 20140623175404) do
 
   create_table "assets", force: true do |t|
     t.string   "url"
