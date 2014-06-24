fb784a002bbe16a3005e7e59604b9f65cb876131
Michael Mitchell <michael.mitchell@getg5.com>
2014-06-24 09:47:20 -0700

    updates schema timestamp

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140623175404) do
+ActiveRecord::Schema.define(version: 20140624162933) do
 
   create_table "assets", force: true do |t|
     t.string   "url"
