5290627f63d0674c0b9c46496ddee6b0c07d3bbd
bbauer <brian.bauer@g5platform.com>
2014-07-25 13:03:20 -0700

    to_json all the things

--- lib/corporate_map_setting.rb
+++ lib/corporate_map_setting.rb
@@ -4,10 +4,10 @@ class CorporateMapSetting
 
   def value
     {
-      "populated_states" => populated_states,
-      "state_location_counts" => state_location_counts,
-      "primary_color" => primary_color,
-      "hover_color" => hover_color
+      "populated_states" => populated_states.to_json,
+      "state_location_counts" => state_location_counts.to_json,
+      "primary_color" => primary_color.to_json,
+      "hover_color" => hover_color.to_json
     }
   end
 
