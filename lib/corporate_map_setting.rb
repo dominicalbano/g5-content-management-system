494d8335500f47e7c6be72d5d38e2b9023898976
bbauer <brian.bauer@g5platform.com>
2014-07-25 11:23:53 -0700

    Calculate primary and hover colors

--- lib/corporate_map_setting.rb
+++ lib/corporate_map_setting.rb
@@ -1,4 +1,46 @@
 class CorporateMapSetting
+  DEFAULT_PRIMARY_COLOR = "#222222"
+  DEFAULT_HOVER_COLOR = "#333333"
+
+  def value
+    {
+      populated_states: populated_states,
+      state_location_counts: state_location_counts,
+      primary_color: primary_color,
+      hover_color: hover_color
+    }
+  end
+
+private
+
+  def primary_color
+    corporate_location_primary_color || DEFAULT_PRIMARY_COLOR
+  end
+
+  def hover_color
+    calculate_hover_color(corporate_location_primary_color) || DEFAULT_HOVER_COLOR
+  end
+
+  def corporate_location_primary_color
+    return unless corporate_location
+    web_theme.custom_primary_color|| web_theme.garden_web_theme.primary_color
+  end
+
+  def calculate_hover_color(base_color)
+    return unless base_color
+    "#%02x%02x%02x" % lighten_hex(base_color.gsub("#",""))
+  end
+
+  def lighten_hex(hex, amount=0.17)
+    rgb = hex.scan(/../).map {|color| color.hex}
+
+    rgb[0] = [(rgb[0].to_i + 255 * amount).round, 255].min
+    rgb[1] = [(rgb[1].to_i + 255 * amount).round, 255].min
+    rgb[2] = [(rgb[2].to_i + 255 * amount).round, 255].min
+
+    rgb
+  end
+
   def populated_states
     grouped_locations.map { |group| group[0] }
   end
@@ -7,20 +49,20 @@ class CorporateMapSetting
     Hash[*states_and_counts.flatten]
   end
 
-  def value
-    {
-      "populated_states" => populated_states,
-      "state_location_counts" => state_location_counts
-    }
-  end
-
-private
-
   def grouped_locations
     Location.order("locations.state ASC").order("locations.city ASC").all.group_by(&:state)
   end
 
   def states_and_counts
     grouped_locations.map { |group| [group[0], group[1].size] }
+  end
+
+  def corporate_location
+    @corporate_location ||= Location.where(corporate: true).first
+  end
+
+  def web_theme
+    return unless corporate_location
+    @web_theme ||= corporate_location.website.website_template.web_theme
   end
 end
--- spec/lib/corporate_map_setting_spec.rb
+++ spec/lib/corporate_map_setting_spec.rb
@@ -5,26 +5,59 @@ describe CorporateMapSetting do
   let!(:location_3) { Fabricate(:location, state: "OR") }
   let!(:location_1) { Fabricate(:location, state: "CA", city: "Los Angeles") }
 
-  describe "#populated_states" do
-    subject { CorporateMapSetting.new.populated_states }
-
-    it "returns an array of states that have locations" do
-      expect(subject).to eq (["CA", "OR"])
-    end
-  end
-
-  describe "#state_location_counts" do
-    subject { CorporateMapSetting.new.state_location_counts }
-
-    it "returns a hash of states and their location counts" do
-      expect(subject).to eq ({ "CA" => 2, "OR" => 1 })
-    end
-  end
-
   describe "#value" do
     subject { CorporateMapSetting.new.value }
 
-    it { is_expected.to eq({ "populated_states" => ["CA", "OR"],
-                             "state_location_counts" => { "CA" => 2, "OR" => 1 } }) }
+    it "displays the correct populated_states" do
+      expect(subject[:populated_states]).to eq(["CA", "OR"])
+    end
+
+    it "displays the correct state_location_counts" do
+      expect(subject[:state_location_counts]).to eq({ "CA" => 2, "OR" => 1 })
+    end
+
+    describe "colors" do
+      context "no corporate defined location" do
+        it "displays the default primary color" do
+          expect(subject[:primary_color]).to eq("#222222")
+        end
+
+        it "displays the default hover color" do
+          expect(subject[:hover_color]).to eq("#333333")
+        end
+      end
+
+      context "a corporate defined location" do
+        let!(:corporate) { Fabricate(:location, corporate: true) }
+        let!(:website) { Fabricate(:website, owner: corporate, website_template: web_template) }
+        let!(:web_theme) { Fabricate(:web_theme, garden_web_theme: garden_web_theme) }
+        let!(:garden_web_theme) { Fabricate(:garden_web_theme, primary_color: "#000000") }
+        let!(:web_template) { Fabricate(:website_template, web_theme: web_theme) }
+
+        context "no custom web_theme colors" do
+          it "displays the default primary color" do
+            expect(subject[:primary_color]).to eq("#000000")
+          end
+
+          it "displays the default hover color" do
+            expect(subject[:hover_color]).to eq("#2b2b2b")
+          end
+        end
+
+        context "custom web_theme colors" do
+          let!(:web_theme) do
+            Fabricate(:web_theme, custom_primary_color: "#FF66FF", garden_web_theme: garden_web_theme)
+          end
+
+          it "displays the default primary color" do
+            expect(subject[:primary_color]).to eq("#FF66FF")
+          end
+
+          it "displays the default hover color" do
+            expect(subject[:hover_color]).to eq("#ff91ff")
+          end
+        end
+      end
+    end
   end
 end
