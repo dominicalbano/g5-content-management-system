3b0fdd246aaf9c7007aa2667403ba86be9bad568
bbauer <brian.bauer@g5platform.com>
2014-07-28 14:21:10 -0700

    Simplify concern

--- app/models/concerns/after_update_set_setting_corporate_map.rb
+++ app/models/concerns/after_update_set_setting_corporate_map.rb
@@ -3,14 +3,10 @@ module AfterUpdateSetSettingCorporateMap
 
   included do
     after_create :update_corporate_map_settings
-    after_update :update_corporate_map_settings, if: :should_update_corporate_map_settings?
+    after_update :update_corporate_map_settings, if: :state_changed?
   end
 
   private
-
-  def should_update_corporate_map_settings?
-    state_changed?
-  end
 
   def update_corporate_map_settings
     Website.all.map(&:update_corporate_map_setting)
