c26753342280262b96c6a2140b15f13dd7b24d40
Don Petersen <don@donpetersen.net>
2014-07-30 21:36:27 +0000

    Truncate long client URNs during CLS JS URL generation.
    More acronyms ASAP.

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -15,4 +15,19 @@ describe ApplicationHelper do
       helper.image_tag('image.jpg').should match "/image.jpg"
     end
   end
+
+  describe "#leads_service_js" do
+    before { helper.stub_chain(:client, urn: urn) }
+    subject { helper.leads_service_js }
+
+    context "with a shorter URN" do
+      let(:urn) { "g5-c-12345-testing" }
+      it { should eq("https://g5-cls-12345-testing.herokuapp.com/assets/form_enhancer.js") }
+    end
+
+    context "with a URN over heroku's app length limit" do
+      let(:urn) { "g5-cls-1skckws2-case-and-associates-properties-inc" }
+      it { should eq("https://g5-cls-1skckws2-case-and-assoc.herokuapp.com/assets/form_enhancer.js") }
+    end
+  end
 end
