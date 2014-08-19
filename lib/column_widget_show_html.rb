64893b06d5826d4de6144763299ca311617ab5c3
bbauer <brian.bauer@g5platform.com>
2014-08-19 12:59:27 -0700

    Further refactor render method

--- lib/column_widget_show_html.rb
+++ lib/column_widget_show_html.rb
@@ -7,7 +7,7 @@ class ColumnWidgetShowHtml < LayoutWidgetShowHtml
     render_widget("row_two_widget_id", "#drop-target-second-row")  if display_two?
     render_widget("row_three_widget_id", "#drop-target-third-row") if display_three?
     render_widget("row_four_widget_id", "#drop-target-fourth-row") if display_four?
-    render_widget("row_five_widget_id", "#drop-target-fifth-row")  if display_five?
+    render_widget("row_five_widget_id", "#drop-target-fifth-row")  if count?("five")
 
     @nokogiri.to_html
   end
@@ -19,18 +19,18 @@ class ColumnWidgetShowHtml < LayoutWidgetShowHtml
   end
 
   def display_two?
-    row_count == "two" || row_count == "three" || row_count == "four" || row_count == "five"
+    count?("two") || count?("three") || count?("four") || count?("five")
   end
 
   def display_three?
-    row_count == "three" || row_count == "four" || row_count == "five"
+    count?("three") || count?("four") || count?("five")
   end
 
   def display_four?
-    row_count == "four" || row_count == "five"
+    count?("four") || count?("five")
   end
 
-  def display_five?
-    row_count == "five"
+  def count?(row)
+    row_count == row
   end
 end
