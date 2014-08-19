8483a2fc6706b2eeabbec4578742e9d8071e9b9a
bbauer <brian.bauer@g5platform.com>
2014-08-19 12:54:06 -0700

    Refactor render method

--- lib/column_widget_show_html.rb
+++ lib/column_widget_show_html.rb
@@ -4,22 +4,10 @@ class ColumnWidgetShowHtml < LayoutWidgetShowHtml
     @nokogiri = Nokogiri.parse(show_html)
 
     render_widget("row_one_widget_id", "#drop-target-first-row")
-
-    if two_rows? or three_rows? or four_rows? or five_rows?
-      render_widget("row_two_widget_id", "#drop-target-second-row")
-
-      if three_rows? or four_rows? or five_rows?
-        render_widget("row_three_widget_id", "#drop-target-third-row")
-
-        if four_rows? or five_rows?
-          render_widget("row_four_widget_id", "#drop-target-fourth-row")
-
-          if five_rows?
-            render_widget("row_five_widget_id", "#drop-target-fifth-row")
-          end
-        end
-      end
-    end
+    render_widget("row_two_widget_id", "#drop-target-second-row")  if display_two?
+    render_widget("row_three_widget_id", "#drop-target-third-row") if display_three?
+    render_widget("row_four_widget_id", "#drop-target-fourth-row") if display_four?
+    render_widget("row_five_widget_id", "#drop-target-fifth-row")  if display_five?
 
     @nokogiri.to_html
   end
@@ -30,19 +18,19 @@ class ColumnWidgetShowHtml < LayoutWidgetShowHtml
     @row_count ||= widget.settings.where(name: "row_count").first.try(:value)
   end
 
-  def two_rows?
-    row_count == "two"
+  def display_two?
+    row_count == "two" || row_count == "three" || row_count == "four" || row_count == "five"
   end
 
-  def three_rows?
-    row_count == "three"
+  def display_three?
+    row_count == "three" || row_count == "four" || row_count == "five"
   end
 
-  def four_rows?
-    row_count == "four"
+  def display_four?
+    row_count == "four" || row_count == "five"
   end
 
-  def five_rows?
+  def display_five?
     row_count == "five"
   end
 end
