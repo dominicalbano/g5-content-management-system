b76f41191d34c368fcd2b275e03d9f0ffbcde2a9
bbauer <brian.bauer@g5platform.com>
2014-08-20 08:27:09 -0700

    Simplify show class

--- lib/column_widget_show_html.rb
+++ lib/column_widget_show_html.rb
@@ -2,28 +2,30 @@ class ColumnWidgetShowHtml < LayoutWidgetShowHtml
   def render
     show_html = Liquid::Template.parse(widget.show_html).render("widget" => widget)
     @nokogiri = Nokogiri.parse(show_html)
+    render_widgets
+    @nokogiri.to_html
+  end
 
+  private
+
+  def render_widgets
     render_widget("row_one_widget_id", "#drop-target-first-row")
     render_widget("row_two_widget_id", "#drop-target-second-row")  if display_two?
     render_widget("row_three_widget_id", "#drop-target-third-row") if display_three?
     render_widget("row_four_widget_id", "#drop-target-fourth-row") if display_four?
     render_widget("row_five_widget_id", "#drop-target-fifth-row")  if count?("five")
-
-    @nokogiri.to_html
   end
-
-  private
 
   def row_count
     @row_count ||= widget.settings.where(name: "row_count").first.try(:value)
   end
 
   def display_two?
-    count?("two") || count?("three") || count?("four") || count?("five")
+    count?("two") || display_three?
   end
 
   def display_three?
-    count?("three") || count?("four") || count?("five")
+    count?("three") || display_four?
   end
 
   def display_four?
