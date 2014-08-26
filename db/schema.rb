a8711796aada614e8c3b23d159942add5957c392
Michael Mitchell <michael.mitchell@getg5.com>
2014-08-26 10:20:14 -0700

    adds organization to client, populates organization on client read

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140725191116) do
+ActiveRecord::Schema.define(version: 20140826171459) do
 
   create_table "assets", force: true do |t|
     t.string   "url"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 20140725191116) do
     t.string   "vertical"
     t.string   "type"
     t.string   "domain"
+    t.string   "organization"
   end
 
   create_table "drop_targets", force: true do |t|
