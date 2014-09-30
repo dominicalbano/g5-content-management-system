25e09c779218ab3714a9ead36268586945d55ef1
Michael Mitchell <michael.mitchell@getg5.com>
2014-09-30 09:47:20 -0700

    fixes savesmanager spec

--- spec/lib/saves_manager_spec.rb
+++ spec/lib/saves_manager_spec.rb
@@ -2,7 +2,11 @@ require 'spec_helper'
 
 describe SavesManager do
   let(:saves_manager) { SavesManager.new("user@email.com") }
-  let(:saves) { open(Rails.root+"spec/fixtures/saves.json").read }
+  let(:saves) { JSON.parse(open(Rails.root+"spec/fixtures/saves.json").read) }
+
+  before do
+    SavesManager.any_instance.stub(:fetch_all).and_return(saves)
+  end
 
   describe "#fetch_all" do
     subject { saves_manager.fetch_all }
@@ -14,20 +18,17 @@ describe SavesManager do
 
       subject { saves_manager.fetch_all.first }
 
-      its(["id"]) { should eq("c83d6988-36b8-42b0-a30a-c0df1d0797f6") }
-      its(["created_at"]) { should eq("2014-01-22T00:16:59Z") }
-    end
-
-    context "bad credentials" do
-      let(:saves) do
-        '{"id":"unauthorized","message":"Invalid credentials provided."}'
-      end
-
-      it { should be_empty }
+      its(["id"]) { should eq("user@foo.com-a257b175-587a-4c75-82e7-d3d2903b3464-DATABASE-URL.dump") }
+      its(["created_at"]) { should eq("2014-01-21T19:30:19Z") }
     end
   end
 
   describe "#restore" do
+    before do
+      expect(GithubHerokuDeployer).to receive(:heroku_run).with("foo")
+      AWS.stub_chain(:s3, :buckets).and_return(saves)
+    end
+
     let(:save_id) { "12-3" }
     let!(:client) {Fabricate(:client)}
 
@@ -35,23 +36,6 @@ describe SavesManager do
       saves_manager.restore(save_id)
     end
 
-    context "a valid location" do
-      let(:website_slug) { location.name.parameterize }
-      let(:heroku_client) { double(restore: nil) }
-
-      before { HerokuClient.stub(new: heroku_client) }
-
-      it "calls restore on HerokuClient" do
-        expect(GithubHerokuDeployer).to receive(:heroku_run).with("foo")
-        subject
-      end
-    end
-
-    context "an in-valid location" do
-      let(:website_slug) { nil }
-
-      it { should be_nil }
-    end
   end
 end
 
