a6e851f45db492c8faa29513f3f8e88edac330db
Michael Mitchell <michael.mitchell@getg5.com>
2014-10-01 11:18:03 -0700

    fixes deploys needing user email

--- app/controllers/websites_controller.rb
+++ app/controllers/websites_controller.rb
@@ -1,7 +1,8 @@
 class WebsitesController < ApplicationController
   def deploy
+    user_email = current_user.email
     @website = Website.find(params[:id])
-    @website.async_deploy
+    @website.async_deploy(user_email)
     redirect_to root_path, notice: "Deploying website #{@website.name}. This may take few minutes."
   end
 end
