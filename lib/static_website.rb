ee82220dc507950b9dff239a79c4e3a83b9882c7
bbauer <brian.bauer@g5platform.com>
2014-10-01 15:17:38 -0700

    Move area page compiler into the website class

--- lib/static_website.rb
+++ lib/static_website.rb
@@ -1,20 +1,14 @@
 require "static_website/compiler"
 require "static_website/deployer"
-require "static_website/compiler/area_pages"
 
 module StaticWebsite
   def self.compile_and_deploy(website, user_email)
     compile(website)
-    compile_area_pages(website) if website.owner.corporate?
     deploy(website, user_email)
   end
 
   def self.compile(website)
     Compiler.new(website).compile
-  end
-
-  def self.compile_area_pages(website)
-    StaticWebsite::Compiler::AreaPages.new(website.compile_path, Website.location_websites).compile
   end
 
   def self.deploy(website, user_email)
