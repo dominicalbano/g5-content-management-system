e2befb64732e6a5f202b8969295b8b66156d5db6
bbauer <brian.bauer@g5platform.com>
2014-10-01 15:07:19 -0700

    Point to the correct class

--- lib/static_website.rb
+++ lib/static_website.rb
@@ -1,5 +1,6 @@
 require "static_website/compiler"
 require "static_website/deployer"
+require "static_website/compiler/area_pages"
 
 module StaticWebsite
   def self.compile_and_deploy(website, user_email)
@@ -13,7 +14,7 @@ module StaticWebsite
   end
 
   def self.compile_area_pages(website)
-    AreaPages.new(website.compile_path, Website.location_websites).compile
+    StaticWebsite::Compiler::AreaPages.new(website.compile_path, Website.location_websites).compile
   end
 
   def self.deploy(website, user_email)
