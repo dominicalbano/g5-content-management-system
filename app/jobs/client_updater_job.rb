f1f3b2c2412040fb87efb3d960ac724c28d42215
bbauer <brian.bauer@g5platform.com>
2014-10-02 09:42:23 -0700

    Add a retry strategy for client updater

--- app/jobs/client_updater_job.rb
+++ app/jobs/client_updater_job.rb
@@ -3,11 +3,16 @@ class ClientUpdaterJob
   @queue = :updater
 
   def self.perform
+    @retries = 0
+
     ClientReader.new(ENV["G5_CLIENT_UID"]).perform
 
     Location.all.each do |location|
       next if location.website.present?
       WebsiteSeeder.new(location).seed
     end
+  rescue
+    @retries += 1
+    Resque.enqueue(ClientUpdaterJob) if @retries < 4
   end
 end
