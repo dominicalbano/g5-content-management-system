ee631b77dda0b25a92a74f69b0cc149e491ec6da
bbauer <brian.bauer@g5platform.com>
2014-10-02 10:53:47 -0700

    Remove retry logic

--- app/jobs/client_updater_job.rb
+++ app/jobs/client_updater_job.rb
@@ -3,15 +3,11 @@ class ClientUpdaterJob
   @queue = :updater
 
   def self.perform
-    tries ||= 3
-
     ClientReader.new(ENV["G5_CLIENT_UID"]).perform
 
     Location.all.each do |location|
       next if location.website.present?
       WebsiteSeeder.new(location).seed
     end
-  rescue
-    retry unless (tries -= 1).zero?
   end
 end
