4a4cc039dc1d904ea280b10d89d4e06058c3153b
bbauer <brian.bauer@g5platform.com>
2014-10-06 08:42:11 -0700

    Remove pending from spec

--- spec/integration/location_page_spec.rb
+++ spec/integration/location_page_spec.rb
@@ -147,8 +147,6 @@ describe "Integration '/:website_slug/:web_page_template_slug'",
       end
 
       it "Creates a new widget in the database and displays in DOM" do
-        pending("Failing for unknown reason: TODO investigate")
-
         garden_widget = find(".widget-list .widgets--list-view .widget:last-of-type")
         drop_target_add = find(".main-widgets .drop-target-add:first-of-type")
         existing_widget_count = all(".main-widgets .widget").length
