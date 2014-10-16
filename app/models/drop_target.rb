d5d368546d126ab2961b21e2d66dca94a7a27d66
bbauer <brian.bauer@g5platform.com>
2014-10-16 10:30:27 -0700

    Destroy DropTarget dependents

--- app/models/drop_target.rb
+++ app/models/drop_target.rb
@@ -1,5 +1,5 @@
 class DropTarget < ActiveRecord::Base
   belongs_to :web_template
-  has_many :widgets, -> { order("widgets.display_order ASC") }
+  has_many :widgets, -> { order("widgets.display_order ASC") }, dependent: :destroy
   delegate :client, to: :web_template
 end
