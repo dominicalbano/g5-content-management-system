f1893e5551451087c582315b861287614e59fae5
Steve Craig <wolfpacksteve@yahoo.com>
2014-10-22 15:37:15 -0700

    made typo - content STRIPE not SLICE

--- db/migrate/20141022001046_rename_rows_widget.rb
+++ db/migrate/20141022001046_rename_rows_widget.rb
@@ -1,6 +1,6 @@
 class RenameRowsWidget < ActiveRecord::Migration
   def change
     row_widget = GardenWidget.find_by_slug('row')
-    row_widget.update(name: 'Content Slice', slug: 'content-slice') if row_widget
+    row_widget.update(name: 'Content Stripe', slug: 'content-stripe') if row_widget
   end
 end
