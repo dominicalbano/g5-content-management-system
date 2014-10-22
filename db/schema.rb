ef6868f3dd83a359c2a49a97a626dfd5f680ceff
Steve Craig <wolfpacksteve@yahoo.com>
2014-10-21 17:19:30 -0700

    add row -> content-stripe migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141021202057) do
+ActiveRecord::Schema.define(version: 20141022001046) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
