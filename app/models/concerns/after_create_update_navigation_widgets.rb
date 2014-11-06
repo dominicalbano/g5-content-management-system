99bb551107ba4a4e8058d18ec9aa0774969ccd12
bbauer <brian.bauer@g5platform.com>
2014-11-06 09:04:24 -0800

    Update nav after creation of nav based widgets

---
+++ app/models/concerns/after_create_update_navigation_widgets.rb
@@ -0,0 +1,23 @@
+module AfterCreateUpdateNavigationWidgets
+  NAVIGATION_WIDGETS = ["Navigation", "Calls To Action"]
+
+  extend ActiveSupport::Concern
+
+  included do
+    after_create :update_nav_settings, if: :should_update?
+  end
+
+  private
+
+  def should_update?
+    NAVIGATION_WIDGETS.include?(garden_widget.name)
+  end
+
+  def update_nav_settings
+    Resque.enqueue(UpdateNavigationSettingsJob, website.id) if website
+  end
+
+  def website
+    WidgetWebsiteFinder.new(self).find
+  end
+end
