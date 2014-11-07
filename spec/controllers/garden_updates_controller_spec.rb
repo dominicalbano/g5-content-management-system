a7619ab4690426a6ce9261fcba63caf5b704ea43
bbauer <brian.bauer@g5platform.com>
2014-11-07 12:09:57 -0800

    Protect the garden updates controller

--- spec/controllers/garden_updates_controller_spec.rb
+++ spec/controllers/garden_updates_controller_spec.rb
@@ -2,32 +2,55 @@ require 'spec_helper'
 
 describe GardenUpdatesController do
   describe "#update" do
+    let(:params) { { "id" => "garden_web_layout" } }
+    let(:token) { double('token') }
+    let(:valid) { false }
+
     before do
+      G5AuthenticatableApi::TokenValidator.any_instance.stub(
+          access_token: token, valid?: valid
+      )
       ResqueSpec.reset!
       put :update, params
     end
 
-    describe "web layout update" do
-      let(:params) { { "id" => "garden_web_layout" } }
+    context "without auth_token" do
+      let(:token) {}
 
-      it "enqueues the GardenWebLayoutUpdaterJob" do
-        expect(GardenWebLayoutUpdaterJob).to have_queue_size_of(1)
+      it "should be redirected" do
+        expect(response.code).to eq('302')
       end
     end
 
-    describe "web theme update" do
-      let(:params) { { "id" => "garden_web_theme" } }
-
-      it "enqueues the GardenWebThemeUpdaterJob" do
-        expect(GardenWebThemeUpdaterJob).to have_queue_size_of(1)
+    context "with invalid auth_token" do
+      it "should return 401" do
+        expect(response.code).to eq('401')
       end
     end
 
-    describe "widget update" do
-      let(:params) { { "id" => "garden_widget" } }
+    context "with valid auth_token" do
+      let(:valid) { true }
 
-      it "enqueues the GardenWidgetUpdaterJob" do
-        expect(GardenWidgetUpdaterJob).to have_queue_size_of(1)
+      describe "web layout update" do
+        it "enqueues the GardenWebLayoutUpdaterJob" do
+          expect(GardenWebLayoutUpdaterJob).to have_queue_size_of(1)
+        end
+      end
+
+      describe "web theme update" do
+        let(:params) { { "id" => "garden_web_theme" } }
+
+        it "enqueues the GardenWebThemeUpdaterJob" do
+          expect(GardenWebThemeUpdaterJob).to have_queue_size_of(1)
+        end
+      end
+
+      describe "widget update" do
+        let(:params) { { "id" => "garden_widget" } }
+
+        it "enqueues the GardenWidgetUpdaterJob" do
+          expect(GardenWidgetUpdaterJob).to have_queue_size_of(1)
+        end
       end
     end
   end
