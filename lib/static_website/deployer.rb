f709bff9d56261e07e98b762696d7f4272b8b11a
Michael Mitchell <michaelmitchell@gmail.com>
2014-11-10 13:55:32 -0800

    list all the directories in app after the deploy happens

--- lib/static_website/deployer.rb
+++ lib/static_website/deployer.rb
@@ -85,6 +85,7 @@ module StaticWebsite
     def clean_up
       LOGGERS.each{|logger| logger.info("Removing directory: #{@repo_dir} if exists")}
       FileUtils.rm_rf(@repo_dir) if @repo_dir && Dir.exists?(@repo_dir)
+      LOGGERS.each{|logger| logger.info(puts Dir.glob("app/**/*/").join("\n"))}
     end
 
     private
