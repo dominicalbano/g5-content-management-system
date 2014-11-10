579afbbf8fec0495469cf879791c493546a8ba3a
Michael Mitchell <michaelmitchell@gmail.com>
2014-11-10 13:55:47 -0800

    list all the directories in tmp after the deploy happens

--- lib/static_website/deployer.rb
+++ lib/static_website/deployer.rb
@@ -86,6 +86,7 @@ module StaticWebsite
       LOGGERS.each{|logger| logger.info("Removing directory: #{@repo_dir} if exists")}
       FileUtils.rm_rf(@repo_dir) if @repo_dir && Dir.exists?(@repo_dir)
       LOGGERS.each{|logger| logger.info(puts Dir.glob("app/**/*/").join("\n"))}
+      LOGGERS.each{|logger| logger.info(puts Dir.glob("tmp/**/*/").join("\n"))}
     end
 
     private
