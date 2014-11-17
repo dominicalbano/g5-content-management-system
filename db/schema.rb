f9111b50018c3be47a652272e3d7209d1c8708b2
bbauer <brian.bauer@g5platform.com>
2014-11-17 11:41:29 -0800

    Add categories table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141105203531) do
+ActiveRecord::Schema.define(version: 20141117194023) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,12 @@ ActiveRecord::Schema.define(version: 20141105203531) do
   create_table "assets", force: true do |t|
     t.string   "url"
     t.integer  "website_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "categories", force: true do |t|
+    t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
