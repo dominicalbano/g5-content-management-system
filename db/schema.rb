d45df7459290805707d8adc11b23a57927f28787
bbauer <brian.bauer@g5platform.com>
2014-11-17 11:53:36 -0800

    Add slug to categories

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141117194023) do
+ActiveRecord::Schema.define(version: 20141117195242) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 20141117194023) do
     t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "slug"
   end
 
   create_table "clients", force: true do |t|
