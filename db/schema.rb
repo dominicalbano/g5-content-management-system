188a5c776a5c51d84095d019b03e7a0ea4c9f895
bbauer <brian.bauer@g5platform.com>
2014-11-17 12:46:50 -0800

    Add category_id to assets

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141117203108) do
+ActiveRecord::Schema.define(version: 20141117204111) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20141117203108) do
     t.integer  "website_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "category_id", default: 1
   end
 
   create_table "categories", force: true do |t|
