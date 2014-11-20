638c3ef8e1748a6bb9a2bd1c6c94bd353cd70948
bbauer <brian.bauer@g5platform.com>
2014-11-20 14:19:54 -0800

    Update specs

--- spec/models/concerns/update_nav_settings_spec.rb
+++ spec/models/concerns/update_nav_settings_spec.rb
@@ -1,11 +1,10 @@
 require "spec_helper"
 
-shared_examples_for AfterCreateUpdateNavigationWidgets do
+shared_examples_for UpdateNavSettings do
   let(:described_instance) { described_class.new(garden_widget: garden_widget) }
+  let(:garden_widget) { Fabricate(:garden_widget, name: widget_name) }
 
   describe ".after_create" do
-    let(:garden_widget) { Fabricate(:garden_widget, name: widget_name) }
-
     context "non navigation based widgets" do
       let(:widget_name) { "HTML" }
 
@@ -33,8 +32,40 @@ shared_examples_for AfterCreateUpdateNavigationWidgets do
       end
     end
   end
+
+  describe ".after_update" do
+    context "non layout based widgets" do
+      let(:widget_name) { "HTML" }
+
+      it "does nothing" do
+        described_instance.should_not_receive(:update_nav_settings)
+        described_instance.save(validate: false)
+        described_instance.update(garden_widget: garden_widget)
+      end
+    end
+
+    context "Column widget" do
+      let(:widget_name) { "Column" }
+
+      it "callls update_nav_settings" do
+        described_instance.should_receive(:update_nav_settings)
+        described_instance.save(validate: false)
+        described_instance.update(garden_widget: garden_widget)
+      end
+    end
+
+    context "Content Stripe widget" do
+      let(:widget_name) { "Content Stripe" }
+
+      it "callls update_nav_settings" do
+        described_instance.should_receive(:update_nav_settings)
+        described_instance.save(validate: false)
+        described_instance.update(garden_widget: garden_widget)
+      end
+    end
+  end
 end
 
 describe Widget do
-  it_behaves_like AfterCreateUpdateNavigationWidgets
+  it_behaves_like UpdateNavSettings
 end
