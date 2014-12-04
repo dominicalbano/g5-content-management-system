bedbad02d7cb9f4297e668a3a56befdb36859f3a
Michael Mitchell <michaelmitchell@gmail.com>
2014-12-04 13:57:14 -0800

    more loggers in singledomain deploy process

--- lib/static_website/compiler/area_page.rb
+++ lib/static_website/compiler/area_page.rb
@@ -19,7 +19,10 @@ module StaticWebsite
       end
 
       def render_to_file
+        LOGGERS.each {|logger| logger.info("opening compile_path")}
         open(compile_path, "wb") do |file|
+          LOGGERS.each {|logger| logger.info("about to try to render to string from #{view_path} with options #{view_options}")}
+          LOGGERS.each {|logger| logger.info("shoveling #{ApplicationController.new.render_to_string(view_path, view_options)} into file")}
           file << ApplicationController.new.render_to_string(view_path, view_options)
         end if compile_path
       end
