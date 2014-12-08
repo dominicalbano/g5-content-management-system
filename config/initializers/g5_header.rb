492493fd4b5b82d53306db186f4bb45d7bc0ec27
bbauer <brian.bauer@g5platform.com>
2014-12-08 13:43:02 -0800

    Configure header in initializer

---
+++ config/initializers/g5_header.rb
@@ -0,0 +1,8 @@
+G5Header.setup do |config|
+  config.app_name = "Content Management System (CMS)"
+  config.dashboard_url = "http://google.com"
+  config.cms_url  = "http://google.com"
+  config.cxm_url = "http://google.com"
+  config.reputation_url  = "http://google.com"
+  config.support_url  = "http://google.com"
+end
