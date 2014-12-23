07c229054418aa27d1637ba9c02a3e63b74ff5b9
Chris Eckert​ <christopher.k.eckert@gmail.com>
2014-12-23 15:30:40 -0800

    refactor widget_cloner.rb

--- spec/fabricators/garden_widget_fabricator.rb
+++ spec/fabricators/garden_widget_fabricator.rb
@@ -19,3 +19,15 @@ Fabricator :row_garden_widget, from: :garden_widget do
   show_html { |attrs| "<div class=\"widget #{attrs[:slug]}\">show</div>" }
   widget_type { "" }
 end
+
+
+Fabricator :html_garden_widget, from: :garden_widget do
+  url { Faker::Internet.url }
+  name { "HTML" }
+  widget_id { 21 }
+  slug { |attrs| attrs[:name].to_s.parameterize }
+  thumbnail { Faker::Internet.url }
+  edit_html { "<div>edit</div>" }
+  show_html { |attrs| "<div class=\"widget #{attrs[:slug]}\">show</div>" }
+  widget_type { "" }
+end
--- spec/lib/cloner/widget_cloner_spec.rb
+++ spec/lib/cloner/widget_cloner_spec.rb
@@ -6,8 +6,10 @@ describe Cloner::WidgetCloner do
   let!(:template) { Fabricate(:web_template, website: website) }
   let!(:target_drop_target) { Fabricate(:drop_target, web_template: template) }
   let!(:row_garden_widget) { Fabricate(:row_garden_widget) }
+
+  let!(:html_garden_widget) { Fabricate(:html_garden_widget) }
   let!(:widget) { Fabricate(:widget, garden_widget: row_garden_widget) }
-  let!(:child_widget) { Fabricate(:widget, drop_target: nil) }
+  let!(:child_widget) { Fabricate(:widget, drop_target: nil, garden_widget: html_garden_widget) }
 
   let!(:widget_id_setting) do
     Fabricate(:setting, owner: widget, name: "column_one_widget_id", value: child_widget.id )
@@ -17,6 +19,8 @@ describe Cloner::WidgetCloner do
     Fabricate(:setting, owner: widget, name: "column_one_widget_name", value: child_widget.name )
   end
 
+
+
   describe "#clone" do
     context "before anything happens" do
       specify { expect(Widget.all.size).to eq(2) }
@@ -25,9 +29,13 @@ describe Cloner::WidgetCloner do
 
     context "after running clone" do
       before do
+        row_garden_widget.settings.clear
+        row_garden_widget.settings << widget_id_setting
+        row_garden_widget.settings << widget_name_setting
+
         widget.settings << widget_id_setting
         widget.settings << widget_name_setting
-        widget_cloner.clone
+        Cloner::WidgetCloner.new(widget, target_drop_target).clone
       end
 
       describe "widget cloning" do
