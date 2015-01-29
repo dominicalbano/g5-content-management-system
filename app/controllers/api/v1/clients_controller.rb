64d04e0cece074665ac8307a4fbb14f736e2582f
Levi Brown <levi.brown@getg5.com>
2015-01-29 12:08:26 -0800

    clients controller remove redirect add message

--- app/controllers/api/v1/clients_controller.rb
+++ app/controllers/api/v1/clients_controller.rb
@@ -6,6 +6,6 @@ class Api::V1::ClientsController < Api::V1::ApplicationController
   def deploy_websites
     user_email = current_user.email
     Client.first.async_deploy(user_email)
-    redirect_to root_path, notice: "Deploying websites. This may take few minutes."
+    render json: {message: "Deploying websites. This may take few minutes."}
   end
 end
