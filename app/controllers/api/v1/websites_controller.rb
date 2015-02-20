8b4b6e071e2440710e2e666f16cabbf95baf00cf
Levi Brown <levi.brown@getg5.com>
2015-02-20 07:53:44 -0800

    update_navigation_setting action

--- app/controllers/api/v1/websites_controller.rb
+++ app/controllers/api/v1/websites_controller.rb
@@ -7,6 +7,12 @@ class Api::V1::WebsitesController < Api::V1::ApplicationController
     render json: Website.all.detect{|website| website.slug == params[:id]}
   end
 
+  def update_navigation_settings
+    website = Website.find(params[:website_id])
+    Resque.enqueue(UpdateNavigationSettingsJob, website.id) if website
+    render json: {message: "Page order Updated."}
+  end
+
   def deploy
     user_email = current_user.email
     @website = Website.find(params[:website_id])
