a38350633231d8cab71fc6c228297d9d6ccc7a37
Levi Brown <levi.brown@getg5.com>
2015-02-23 08:45:52 -0800

    compile app | compile assets

--- app/controllers/api/v1/application_controller.rb
+++ app/controllers/api/v1/application_controller.rb
@@ -2,6 +2,17 @@ class Api::V1::ApplicationController < ActionController::Base
   prepend_before_filter :authenticate_api_user!, unless: proc{ Rails.env.development? }
   before_filter :set_headers, if: proc{ Rails.env.development? }
 
+  alias_method :devise_current_user, :current_user
+  def current_user
+    # hack for external cms ember app development
+    # TODO move auth to ember app
+    if Rails.env.development? && devise_current_user.nil?
+      OpenStruct.new(email: "foo@example.com")
+    else
+      devise_current_user
+    end   
+  end
+
   def set_headers
     headers['Access-Control-Allow-Origin'] = '*'
     headers['Access-Control-Expose-Headers'] = 'ETag'
