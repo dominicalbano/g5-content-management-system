02d2a0e340f89ac36990bd065492aa2c960c6074
Steve Craig <steve.craig@getg5.com>
2015-02-28 11:15:42 -0600

    simplify position_name in seeder

--- lib/seeders/layout_widget_seeder.rb
+++ lib/seeders/layout_widget_seeder.rb
@@ -22,24 +22,13 @@ class LayoutWidgetSeeder < ModelSeeder
 
   def set_default_widget_settings(layout, widgets)
     @widget.settings.find_by_name(layout_var).update_attributes(layout)
-    widgets.try(:each_with_index) do |instruction, idx|
+    widgets.each_with_index(1) do |instruction, idx|
       w = WidgetSeeder.new(nil, instruction)
       if w.valid?
-        pos = position_name(idx)
+        pos = position_name[idx]
         @widget.settings.find_by_name("#{position_var}_#{pos}_widget_name").update_attributes(w.name)
         @widget.settings.find_by_name("#{position_var}_#{pos}_widget_id").update_attributes(w.id)
       end
-    end
-  end
-
-  def position_name(index)
-    case index
-      when 0 return 'one'
-      when 1 return 'two'
-      when 2 return 'three'
-      when 3 return 'four'
-      when 4 return 'five'
-      when 5 return 'six'
     end
   end
 end
