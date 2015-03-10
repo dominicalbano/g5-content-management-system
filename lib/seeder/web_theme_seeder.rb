217b367a7e52878975782958e7e96e75b91f0de7
Steve Craig <steve.craig@getg5.com>
2015-03-10 10:01:52 -0600

    add web theme seeder spec

--- lib/seeder/web_theme_seeder.rb
+++ lib/seeder/web_theme_seeder.rb
@@ -3,18 +3,23 @@ module Seeder
   class WebThemeSeeder < Seeder::ModelSeeder
     attr_reader :instructions, :template, :website
 
-    def initialize(website, template, instructions)
-      @website = website
+    def initialize(template, instructions)
       @template = template
       @instructions = instructions
     end
 
     def seed
+      raise SyntaxError unless has_valid_instructions?
       @template.create_web_theme(theme_params)
       @template
     end
     
     private
+
+    def has_valid_instructions?
+      @template && @instructions &&
+      @instructions.has_key?(:slug)
+    end
 
     def theme_params
       params_for(GardenWebTheme, @instructions, :garden_web_theme_id)
---
+++ spec/lib/seeder/web_theme_seeder_spec.rb
@@ -0,0 +1,45 @@
+require "spec_helper"
+
+describe Seeder::WebThemeSeeder do
+  let!(:client) { Fabricate(:client) }
+  let!(:location) { Fabricate(:location) }
+  let(:website) { Fabricate(:website, owner: location) }
+  let(:website_template) { Fabricate(:website_template, website: website) }
+  let(:defaults) { HashWithIndifferentAccess.new(YAML.load_file("#{Rails.root}/config/defaults/websites/defaults.yml")) }
+  let(:instructions) { defaults[:website_template][:web_theme] }
+  let(:seeder) { Seeder::WebThemeSeeder.new(website_template, instructions) }
+
+  describe "#seed" do
+    let(:web_theme_name) { GardenWebTheme.find_by_slug(instructions[:slug]) }
+    subject { seeder.seed }
+
+    context "valid instructions" do
+      before { subject }
+
+      it "creates a web theme for the web template" do
+        expect(website_template.web_theme.name).to eq(web_theme_name)
+      end
+    end
+
+    context "no instructions" do
+      let(:instructions) { nil }
+      it "raises a SyntaxError" do
+        expect { subject }.to raise_error(SyntaxError)
+      end
+    end
+
+    context "no website template" do
+      let(:website_template) { nil }
+      it "raises a SyntaxError" do
+        expect { subject }.to raise_error(SyntaxError)
+      end
+    end
+
+    context "invalid instructions" do
+      before { instructions.except!(:slug) }
+      it "raises a SyntaxError" do
+        expect { subject }.to raise_error(SyntaxError)
+      end
+    end
+  end
+end
