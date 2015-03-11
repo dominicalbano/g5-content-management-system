8c4eac03459fc9fe0032307e9e2cff122512f020
Steve Craig <steve.craig@getg5.com>
2015-03-11 08:12:29 -0600

    add liquid support and validation to garden widget updater spec and support files

--- spec/fabricators/garden_widget_fabricator.rb
+++ spec/fabricators/garden_widget_fabricator.rb
@@ -4,6 +4,7 @@ Fabricator :garden_widget do
   widget_id { 1 }
   slug { |attrs| attrs[:name].to_s.parameterize }
   thumbnail { Faker::Internet.url }
+  liquid { false }
   edit_html { "<div>edit</div>" }
   show_html { |attrs| "<div class=\"widget #{attrs[:slug]}\">show</div>" }
   widget_type { "" }
@@ -15,6 +16,7 @@ Fabricator :row_garden_widget, from: :garden_widget do
   name { "Content Stripe" }
   widget_id { 11 }
   slug { |attrs| attrs[:name].to_s.parameterize }
+  liquid { false }
   thumbnail { Faker::Internet.url }
   edit_html { "<div>edit</div>" }
   show_html { |attrs| "<div class=\"widget #{attrs[:slug]}\">show</div>" }
@@ -28,6 +30,7 @@ Fabricator :html_garden_widget, from: :garden_widget do
   widget_id { 21 }
   slug { |attrs| attrs[:name].to_s.parameterize }
   thumbnail { Faker::Internet.url }
+  liquid { false }
   edit_html { "<div>edit</div>" }
   show_html { |attrs| "<div class=\"widget #{attrs[:slug]}\">show</div>" }
   widget_type { "" }
@@ -39,6 +42,7 @@ Fabricator :analytics_garden_widget, from: :garden_widget do
   widget_id { 1 }
   slug { |attrs| attrs[:name].to_s.parameterize }
   thumbnail { Faker::Internet.url }
+  liquid { false }
   edit_html { "<div>edit</div>" }
   show_html { |attrs| "<div class=\"widget #{attrs[:slug]}\">show</div>" }
   widget_type { "" }
