7069ce99c3fc1620194a2acb1370b3348ebaf7ba
Steve Craig <steve.craig@getg5.com>
2015-03-15 18:18:51 -0700

    resolve bug in integration support spec

--- spec/support/integration_support.rb
+++ spec/support/integration_support.rb
@@ -58,7 +58,7 @@ def seed(file="example.yml")
   client = Fabricate(:client)
   location = Fabricate(:location)
   instructions = YAML.load_file("#{Rails.root}/spec/support/website_instructions/#{file}")
-  website = WebsiteSeeder.new(location, instructions).seed
+  website = Seeder::WebsiteSeeder.new(location, instructions).seed
   website.assets << Fabricate(:asset)
   [client, location, website]
 end
