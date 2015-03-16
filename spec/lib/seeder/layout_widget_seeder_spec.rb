1fadb6c8b02dc38e06e1cb6df2872340f6b4c1b5
Steve Craig <steve.craig@getg5.com>
2015-03-16 12:35:01 -0700

    test spec on nested widgets

--- spec/lib/seeder/layout_widget_seeder_spec.rb
+++ spec/lib/seeder/layout_widget_seeder_spec.rb
@@ -38,13 +38,13 @@ describe Seeder::LayoutWidgetSeeder do
             expect(@response.get_setting('row_layout').value).to eq(instructions[:row_layout])
           end
         ##TODO: figure out why this doesn't work!
-        #  it "creates correct nested widget settings for content stripe widget" do
-        #    expect(@response.child_widgets.size).to eq(instructions[:widgets].size)
+          it "creates correct nested widget settings for content stripe widget" do
+            expect(@response.child_widgets.size).to eq(instructions[:widgets].size)
         #    expect(@response.child_widgets.first.slug).to eq(instructions[:widgets].first[:slug])
         #    expect(@response.child_widgets.second.slug).to eq(instructions[:widgets].second[:slug])
-        #    expect(@response.get_setting_value('column_one_widget_name')).to eq(html_widget.name)
-        #    expect(@response.get_setting_value('column_two_widget_name')).to eq(map_widget.name)
-        #  end
+            expect(@response.get_setting_value('column_one_widget_name')).to eq(html_widget.name)
+            expect(@response.get_setting_value('column_two_widget_name')).to eq(map_widget.name)
+          end
         end
       end
 
