48ff29ceb5b73948a6da87d1db7bc47ad1fd3fff
Michael Mitchell <michaelmitchell@gmail.com>
2015-03-17 14:27:29 -0700

    fix path

--- lib/static_website/deployer.rb
+++ lib/static_website/deployer.rb
@@ -66,7 +66,7 @@ module StaticWebsite
       FileUtils.cp_r(File.join(Rails.root, "public", "javascripts") + "/.", @repo_dir + "/javascripts")
       FileUtils.cp(File.join(Rails.root, "public", "area_page.js"), @repo_dir + "/javascripts/area_page.js")
 
-      FileUtils.cp(File.join(Rails.root, ActionController::Base.helpers.asset_path("area_page.css")), "public", @repo_dir + ActionController::Base.helpers.asset_path('area_page.css'))
+      FileUtils.cp(File.join(Rails.root, 'public', ActionController::Base.helpers.asset_path("area_page.css")), @repo_dir + ActionController::Base.helpers.asset_path('area_page.css'))
 
       Rails.logger.debug("git config name, email")
       repo.config('user.name', ENV['HEROKU_APP_NAME']) 
