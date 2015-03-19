93388925a683905f3bbf2b4d0b5b19fa0986fe9d
maeve <maeve.revels@getg5.com>
2015-03-19 22:24:33 +0000

    Increase the max time capybara will wait for async js

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -90,3 +90,4 @@ Capybara.register_driver :poltergeist do |app|
   Capybara::Poltergeist::Driver.new(app, timeout: 120)
 end
 Capybara.javascript_driver = :poltergeist
+Capybara.default_wait_time = 5
