434b754ba72c4325b70473175bd4b071b46c6bf8
maeve <maeve.revels@getg5.com>
2015-03-20 01:42:37 +0000

    PhantomJS debugging options cause more frequent test failures

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -87,9 +87,7 @@ require 'capybara/poltergeist'
 Capybara.register_driver :poltergeist do |app|
   # Default timeout is 30, but that causes sporadic timeout errors
   # on the CI server
-  Capybara::Poltergeist::Driver.new(app, timeout: 120,
-                                         debug: true,
-                                         phantomjs_options: ['--load-images=no', '--debug=true'])
+  Capybara::Poltergeist::Driver.new(app, timeout: 180)
 end
 Capybara.javascript_driver = :poltergeist
 Capybara.default_wait_time = 5
