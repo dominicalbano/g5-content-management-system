a2abdbfe82099ff437caaee49335f21fe2de0a42
Steve Craig <steve.craig@getg5.com>
2015-03-23 15:55:03 -0700

    tweak website seeder job spec

--- spec/jobs/website_seeder_job_spec.rb
+++ spec/jobs/website_seeder_job_spec.rb
@@ -3,7 +3,8 @@ require "spec_helper"
 describe WebsiteSeederJob do
   let!(:location) { Fabricate(:location) }
   let!(:location2) { Fabricate(:location) }
-  let!(:website) { Fabricate(:website, owner: location2, urn: 'test') }
+  let!(:location3) { Fabricate(:location) }
+  let!(:website) { Fabricate(:website, owner: location) }
   let(:website_seeder_job) { double(perform: nil) }
 
   describe "#self.perform" do
@@ -13,19 +14,20 @@ describe WebsiteSeederJob do
     subject { described_class.perform }
 
     context "a location without a website" do
-      it "instantiates a new job with the location" do
-        described_class.should_receive(:new).exactly(1).times
+      it "instantiates a new job with the locations" do
+        described_class.should_receive(:new).exactly(2).times
       end
 
-      it "performs website seeder for the location" do
+      it "performs website seeder for the locations" do
         website_seeder_job.should_receive(:perform)
       end
     end
 
     context "a location with a website" do
-      before { location.destroy }
+      let!(:website2) { Fabricate(:website, owner: location2) }
+      let!(:website3) { Fabricate(:website, owner: location3) }
 
-      it "does not instantiate a new job with the location" do
+      it "does not instantiate a new job with the locations" do
         described_class.should_receive(:new).exactly(0).times
       end
 
@@ -35,8 +37,7 @@ describe WebsiteSeederJob do
     end
 
     context "a location by URN - replace website" do
-      let(:location_urn) { location2.urn }
-      before { location.destroy }
+      let(:location_urn) { location.urn }
       subject { described_class.perform(location_urn) }
 
       it "instantiates a new job with the location" do
