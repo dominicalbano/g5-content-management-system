d1c92e3e45ba2a4b0051b8d7fb07eff9f35e1f82
Michael Mitchell <michaelmitchell@gmail.com>
2015-03-23 12:02:09 -0700

    simplifies copying widget setting to website setting

--- app/models/concerns/setting_navigation.rb
+++ app/models/concerns/setting_navigation.rb
@@ -1,5 +1,4 @@
-module SettingNavigation
-  extend ActiveSupport::Concern
+module SettingNavigation extend ActiveSupport::Concern
 
   included do
     before_update :update_widget_navigation_setting, if: :widget_navigation_setting_updated?
@@ -43,26 +42,16 @@ module SettingNavigation
   end
 
   def create_new_value(website_value, widget_value)
-    new_value = {}
-    website_value.each_pair do |key, website_page_value|
-      widget_page_value = widget_value[key]
-      # widget_page_value could be false, so can't check presence
-      unless widget_page_value.nil?
-        website_page_value["display"] = widget_page_value["display"]
-        if child_templates = website_page_value.fetch("child_templates", false)
-          child_templates.each_pair do |key, child_template_value|
-            child_template_value["display"] = widget_page_value["child_templates"][key]["display"] if widget_page_value.fetch("child_templates", false)
-          end
-          website_page_value["sub_nav"] = true if show_sub_nav?(website_page_value)
-        end
-      end
-      new_value[key] = HashWithToLiquid[website_page_value]
+    website_value.deep_merge!(widget_value)
+    website_value["sub_nav"] = true if show_sub_nav?(website_value)
+    website_value.keys.each do |key|
+      website_value[key]["sub_nav"] = "true" if show_sub_nav?(website_value[key])
     end
-    new_value
+    website_value
   end
   
-  def show_sub_nav?(website_page_value)
-    website_page_value["child_templates"].any? {|child| child[1]["display"] == "true"}
+  def show_sub_nav?(website_value)
+    website_value.fetch("child_templates",[]).any? {|child| child[1]["display"] == "true"}
   end
 
   def orphaned_drop_target?
@@ -76,4 +65,6 @@ module SettingNavigation
   def setting_website
     website ||= WebsiteFinder::Setting.new(self).find
   end
+
 end
+
