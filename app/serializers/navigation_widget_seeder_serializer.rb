fff51c19600e80b84a7919266500f7f5afba2c44
Steve Craig <steve.craig@getg5.com>
2015-04-10 09:34:21 -0700

    add new seeder serializers for widgets, update mf default seeder files

--- app/serializers/navigation_widget_seeder_serializer.rb
+++ app/serializers/navigation_widget_seeder_serializer.rb
@@ -1,7 +1,15 @@
 class NavigationWidgetSeederSerializer < ExtendedWidgetSeederSerializer
+  def default_display_navigation
+    false
+  end
+
   protected
 
   def extended_settings_list
-    ['display_navigation', 'display_as_calls_to_action', 'display_corporate_navigation']
+    ['display_as_calls_to_action', 'display_corporate_navigation']
+  end
+
+  def default_settings_list
+    ['display_navigation']
   end
 end
