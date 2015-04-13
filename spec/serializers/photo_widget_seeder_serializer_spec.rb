445cb3449dd47f2185fc9fdd7f03c965147ffadd
Steve Craig <steve.craig@getg5.com>
2015-04-13 12:02:25 -0700

    update photo serializer spec to include placehold.it fallbacks for columns

--- spec/serializers/photo_widget_seeder_serializer_spec.rb
+++ spec/serializers/photo_widget_seeder_serializer_spec.rb
@@ -38,6 +38,54 @@ describe PhotoWidgetSeederSerializer do
         end
 
         context "has parent content stripe" do
+          let(:cs_garden_widget) { Fabricate(:content_stripe_garden_widget) }
+          let(:cs_widget) { Fabricate(:widget, garden_widget: cs_garden_widget, drop_target: drop_target) }
+          before do
+            widget.drop_target = nil
+            widget.save
+            cs_widget.set_child_widget(1, widget)
+          end
+
+          context "is single layout" do
+            it "returns the max sized placehold.it image" do
+              expect(subject[:settings].first[:value]).to eq(single)
+            end
+          end
+
+          context "is halves layout" do
+            before { cs_widget.set_setting(cs_widget.layout_var, 'halves') }
+            it "returns the halves sized placehold.it image" do
+              expect(subject[:settings].first[:value]).to eq(halves)
+            end
+          end
+
+          context "is thirds layout" do
+            before { cs_widget.set_setting(cs_widget.layout_var, 'thirds') }
+            it "returns the thirds sized placehold.it image" do
+              expect(subject[:settings].first[:value]).to eq(thirds)
+            end
+          end
+
+          context "is fourths layout" do
+            before { cs_widget.set_setting(cs_widget.layout_var, 'fourths') }
+            it "returns the fourths sized placehold.it image" do
+              expect(subject[:settings].first[:value]).to eq(fourths)
+            end
+          end
+
+          context "is uneven thirds 1 layout" do
+            before { cs_widget.set_setting(cs_widget.layout_var, 'uneven-thirds-1') }
+            it "returns the thirds sized placehold.it image" do
+              expect(subject[:settings].first[:value]).to eq(thirds)
+            end
+          end
+
+          context "is uneven thirds 2 layout" do
+            before { cs_widget.set_setting(cs_widget.layout_var, 'uneven-thirds-2') }
+            it "returns the two-thirds sized placehold.it image" do
+              expect(subject[:settings].first[:value]).to eq(two_thirds)
+            end
+          end
         end
       end
 
