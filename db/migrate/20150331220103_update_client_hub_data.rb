7a7336fa5748f26b51680ce5d5ab13ce98990198
Steve Craig <steve.craig@getg5.com>
2015-04-13 13:52:27 -0700

    update migration to use resque

--- db/migrate/20150331220103_update_client_hub_data.rb
+++ db/migrate/20150331220103_update_client_hub_data.rb
@@ -1,6 +1,6 @@
 class UpdateClientHubData < ActiveRecord::Migration
   def change
     ClientReader.new(ENV["G5_CLIENT_UID"]).perform
-    #GardenWidgetUpdater.new.update_all(true)
+    Resque.enqueue(GardenWidgetUpdaterJob, true)
   end
 end
