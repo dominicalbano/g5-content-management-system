d690d2d023c07dab8e58d1825cdb4c414b6388f1
Chris Eckert​ <christopher.k.eckert@gmail.com>
2015-04-15 11:39:28 -0700

    remove Client Deployer. Replace with multiple location deployers

--- lib/client_deployer/website_compiler.rb
+++
@@ -1,7 +0,0 @@
-require "client_deployer/website_compiler/website"
-
-module ClientDeployer::WebsiteCompiler
-  def self.new(website)
-    ClientDeployer::WebsiteCompiler::Website.new(website)
-  end
-end
